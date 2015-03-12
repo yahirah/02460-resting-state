2a2f863f4bfd168b9f16a4052ad195c79d140474
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-03-12 16:38:32 +0100

    Loading the fmri file.

--- Exercise/Ania/ex.py
+++ Exercise/Ania/ex.py
@@ -5,4 +5,9 @@ print mat.keys()
 print mat['EEGdata'].dtype
 print mat['EEGdata']['R128']
 print mat['EEGdata']['Fs'][0][0][0][0]
-print mat['EEGdata']['data'][0][0]
+print mat['EEGdata']['data'][0][0].shape
+
+with open('t1.txt') as f:
+    array = [float(line) for line in f]
+
+print len(array)
