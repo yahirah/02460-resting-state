acef82197cf4cd0b930d8d5df36d51bed2524e5e
deoxyribose <frzdyb@gmail.com>
2015-03-26 16:38:10 +0100

    13 ICs and labels

--- Exercise/Frans/plot_microstates.py
+++ Exercise/Frans/plot_microstates.py
@@ -15,10 +15,12 @@ eeg = data['data'][0][0]
 
 chanlocs = loadmat("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/chanlocs.mat", squeeze_me=True)
 coords = np.zeros((30,2))
+channames = []
 
 for channel in range(chanlocs['chanlocs'].shape[0]-2):
     coords[channel,0] = chanlocs['chanlocs'][channel][4]
     coords[channel,1] = chanlocs['chanlocs'][channel][5]
+    channames.append(chanlocs['chanlocs'][channel][0])
 
 rot = np.matrix([[0,-1],[1,0]])
 rot_coords = rot*coords.T
@@ -41,18 +43,30 @@ xi = np.linspace(-90, 90, 100)
 yi = np.linspace(-90, 90, 200)
 for i in range(1,17):
     ax = plt.subplot(4,4,i)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    plt.xlim(-90, 90)
+    plt.ylim(-90, 90)
     # grid the data.
     if i <= 13:
         zi = griddata(coords[:,0], coords[:,1], A[:,i-1], xi, yi, interp='linear')
         # contour the gridded data, plotting dots at the nonuniform data points.
-        CS = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
-        CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow, vmax=abs(zi).max(), vmin=-abs(zi).max())
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
+        ax.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
+        ax.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow, vmax=abs(zi).max(), vmin=-abs(zi).max())
 #        plt.colorbar()  # draw colorbar
         # plot data points.
-        plt.scatter(coords[:,0], coords[:,1], marker='o', c='b', s=5, zorder=10)
-        plt.xlim(-90, 90)
-        plt.ylim(-90, 90)
-plt.title('Microstates?')
-plt.savefig("spatial_white_13.png")
+        for c in range(len(channames)):
+            ax.scatter(coords[c,0], coords[c,1], marker=r"$ {} $".format(channames[c]) , c='b', s=50, zorder=10, edgecolors='none')
+    plt.title('IC%d' %(i))
+#plt.title('Microstates?')
+plt.savefig("spatial_white_13.png",dpi=600)
+
+plt.clf()
+for i in range(13):
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax = plt.subplot(13,1,i+1)
+    ax.plot(range(S.shape[0]),S[:,i],linewidth=0.2)
+    plt.title('IC%d' %(i+1))
+#plt.title('Temporal components')
+plt.savefig("temporal_white_13.png",dpi=600)
