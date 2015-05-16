b9713f051e7a5d66231c1bea60e611cdec0bc5ae
deoxyribose <frzdyb@gmail.com>
2015-05-16 14:42:29 +0200

    matlab pipeline

---
+++ Exercise/Frans/init.m
@@ -0,0 +1,139 @@
+addpath(genpath('/home/frans/MATLAB packages/eeglab13_4_4b/'))
+addpath(genpath('/home/frans/MATLAB packages/FastICA_25/'))
+
+% pipeline:
+% load data for 5 subjects
+% make leave-one-subject-out cross-validation to estimate {1. robustness of
+% found microstates, 2. length of microstate dominance intervals}
+% for each fold
+    % find peaks
+    % do ICA
+    % order by explained variance
+% find similarity between found microstates across folds
+load('/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/chanlocs.mat')
+%% load eeg data
+eeg = zeros(300000,30,5);
+load('/home/frans/Dropbox/filteredICA/20110926x3_EEG_ATM_filtICA.mat');
+eeg(1:size(X,2),:,1) = X';
+load('/home/frans/Dropbox/filteredICA/20111024x3_EEG_ATM_filtICA.mat');
+eeg(1:size(X,2),:,2) = X';
+load('/home/frans/Dropbox/filteredICA/20111121x5_EEG_ATM_filtICA.mat');
+eeg(1:size(X,2),:,3) = X';
+load('/home/frans/Dropbox/filteredICA/20120213x5_EEG_ATM_filtICA.mat');
+eeg(1:size(X,2),:,4) = X';
+load('/home/frans/Dropbox/filteredICA/20120312x6_EEG_ATM_filtICA.mat');
+eeg(1:size(X,2),:,5) = X';
+% eeg = zeros(300000,30,18);
+% files = dir('/home/frans/documents/AdvancedMachineLearningStuff/filtered/*.mat');
+% for k = 1:length(files)
+%     load(strcat('/home/frans/documents/AdvancedMachineLearningStuff/filtered/',files(k).name));
+%     size(EEGdata.filtered_data,1)
+%     eeg(1:size(EEGdata.filtered_data,1),:,k) = EEGdata.filtered_data;
+% end
+%% load fmri data
+Y = zeros(3000,60);
+files = dir('/home/frans/documents/AdvancedMachineLearningStuff/fmriics/*.txt');
+mask = [((4-1)*600+1):(6*600) ((9-1)*600+1):(10*600)]; % 4,5,6,9,10
+for k = 1:length(files)
+    y = csvread(strcat('/home/frans/documents/AdvancedMachineLearningStuff/fmriics/',files(k).name));
+    tal = regexp(files(k).name,['\d+'],'match');
+    %y(((11-1)*600+1):(11*600)) = []; % array is now 600 smaller, so subject 14 is now 13
+    %y(((13-1)*600+1):(13*600)) = []; 
+    y = y(mask);
+    % the last 8 subjects had first 3 seconds removed from eeg
+    %for i=10:17
+    %    y((i*600+1):(i*600+3)) = [];
+    %end
+    str2num(tal{1});
+    Y(:,str2num(tal{1})) = y;
+end
+%% do ICA on peak topographies
+eegfold = [];
+for subject=1:size(eeg,3)
+    eegfold = cat(1,eegfold,eeg(:,:,subject));
+end
+
+gfp = std(eegfold,0,2);
+[~, locs] = findpeaks(gfp,'MinPeakDist',50);
+    % findpeaks(gfp(1:1500)),'MinPeakWidth',3,'MaxPeakWidth',60);
+microstates = eegfold(locs,:)';
+[A, W] = fastica(microstates);%,'approach','symm'); % using cubic polynomial
+
+S = W*eegfold'; % apply seperation matrix to original data
+variance_of_component = zeros(30,1);
+for i=1:30
+    back_proj = (A(:,i)*S(i,:))';
+    variance_of_component(i) = 1 - var(eegfold(:) - back_proj(:))/var(eegfold(:));
+end
+[~, order1] = sort(variance_of_component);
+
+figure()
+for i=1:30
+    subplot(5,6,order1(i))
+    topoplot(A(:,i),chanlocs,'electrodes','off','style','map');%,'plotchans',chanlocs(1:30).labels);
+end
+
+Asorted = A(:,order1);
+%%
+As = zeros(30,30,5);
+for fold=1:5
+    tmp = 1:5;
+    tmp(fold) = [];
+    eegfold = [];
+    for subject=tmp
+        eegfold = cat(1,eegfold,eeg(:,:,subject));
+    end
+    gfp = std(eegfold,0,2);
+    [~, locs] = findpeaks(gfp,'MinPeakDist',50);
+    microstates = eegfold(locs,:)';
+    [icasig, A, W] = fastica(microstates); % using cubic polynomial
+    S = W*eegfold'; % apply seperation matrix to original data
+    variance_of_component = zeros(30,1);
+    for i=1:30
+        back_proj = (A(:,i)*S(i,:))';
+        variance_of_component(i) = 1 - var(eegfold(:) - back_proj(:))/var(eegfold(:));
+    end
+    [~, order1] = sort(variance_of_component);
+    As(:,:,fold) = A(:,order1);
+end
+%%
+for j=1:5
+    figure()
+    for i=1:30
+        subplot(5,6,i)
+        topoplot(As(:,i,j),chanlocs,'electrodes','off','style','map');%,'plotchans',chanlocs(1:30).labels);
+    end
+end
+%% try kmeans
+[idx C] = kmeans(microstates',30,'MaxIter',1000);
+for i=1:30
+    subplot(5,6,order1(i))
+    topoplot(C(i,:),chanlocs);
+end
+%% calculate variance explained by each component
+variance_of_component = zeros(30,1);
+for i=1:30
+    back_proj = (A(:,i)*S(i,:))';
+    variance_of_component(i) = 1 - var(eeg(:) - back_proj(:))/var(eeg(:));
+end
+[~, order] = sort(variance_of_component);
+%%
+idx = 1:2000:95000;
+figure()
+c = 1
+for i=idx
+    subplot(5,10,c)
+    topoplot(microstates(:,i),chanlocs);
+    c = c+1;
+end
+%% plot temporal components
+% should be same kind of plot as yuan appendix C
+figure()
+subplot(4,1,1)
+plot(gfp(1:250))
+c = 2;
+for i=1:3
+    subplot(4,1,c)
+    plot(abs(S(i,1:250)))
+    c = c+1;
+end
--- Exercise/Frans/pipeline.py
+++ Exercise/Frans/pipeline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import itertools
 from scipy.io import loadmat
 import scipy.signal as signal
 from sklearn import preprocessing
@@ -7,12 +8,13 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import FastICA
 from matplotlib.mlab import griddata
 
+
 def open_file(path):
     # retrieve data
     data = loadmat(path, squeeze_me=False)
     data = data['EEGdata']
     idx = data['R128'].flatten()[0].flatten()
-    eeg = data['filtered_data'][0][0][(idx[0]+1500):]
+    eeg = data['filtered_data'][0][0]
 
     no_electrodes = eeg.shape[1] # - 2 
     print "Filtered data loaded"
@@ -20,25 +22,59 @@ def open_file(path):
     print "Shape of eeg array: " + str(eeg.shape)
     return eeg, no_electrodes
 
-gen_path = "/home/frans/documents/eegdata/"
-paths = ["20110822x4_EEG_ATM_filt"]
-path = paths[0]
+def open_file2(path):
+    # retrieve data
+    data = loadmat(path, squeeze_me=False)
+    data = data['X']
+    #idx = data['R128'].flatten()[0].flatten()
+    eeg = data.T
+
+    no_electrodes = eeg.shape[1] # - 2 
+    print "Filtered data loaded"
+    print "No of electrodes: " + str(no_electrodes)
+    print "Shape of eeg array: " + str(eeg.shape)
+    return eeg, no_electrodes
+
+gen_path = "/home/frans/Dropbox/filteredICA/"
+#paths = ["20110822x4_EEG_ATM_filt", "20110823x1_EEG_ATM_filt", "20110912x4_EEG_ATM_filt",
+#         "20110926x3_EEG_ATM_filt", "20111024x3_EEG_ATM_filt", "20111121x5_EEG_ATM_filt",
+#         "20111205x6_EEG_ATM_filt", "20120130x5_EEG_ATM_filt", "20120213x5_EEG_ATM_filt",
+#         "20120312x6_EEG_ATM_filt", "20130819x2_EEG_ATM_filt", "20130826x1_EEG_ATM_filt",
+#         "20130903x1_EEG_ATM_filt", "20130908x1_EEG_ATM_filt", "20130908x2_EEG_ATM_filt",
+#         "20130908x3_EEG_ATM_filt", "20131114x1_EEG_ATM_filt", "20131114x2_EEG_ATM_filt"]
+paths = ["20110926x3_EEG_ATM_filtICA", "20111024x3_EEG_ATM_filtICA", "20111121x5_EEG_ATM_filtICA",
+        "20120213x5_EEG_ATM_filtICA", "20120312x6_EEG_ATM_filtICA"]
+
+#paths = paths[0:1]
+# 11 and 14 don't have corresponding dmn, so remove 2000-2200 and 2600-2800 from y
+
+i = 0
+
 for path in paths:
     print "*** Path no " + str(paths.index(path) + 1) + " of " + str(len(paths))
     p = gen_path + path;
-    eeg, no_electrodes = open_file(p)
-    eeg = preprocessing.scale(eeg) # mean 0, std 1
+    eeg, no_electrodes = open_file2(p)
+    #eeg = eeg[0:250,:] # 500 ms
+    #eeg = preprocessing.scale(eeg) # mean 0, std 1
     gfp = np.std(eeg, axis=1)
     peaks = signal.find_peaks_cwt(gfp, np.arange(1,15),noise_perc=50)
-    microstates = eeg[peaks,:]
+    if i == 0:
+        eeg_all = eeg
+        microstates = eeg[peaks,:]
+    else:
+        eeg_all = np.concatenate((eeg_all,eeg), axis=0)
+        microstates = np.concatenate((microstates,eeg[peaks,:]), axis=0)
+    i += 1
 
 #np.savetxt("microstates.txt", microstates)
-#np.savetxt("eeg_standarized.txt", eeg)
+#np.savetxt("eeg_NONstandarized.txt", eeg_all)
 
-#plt.clf()
-#plt.plot(gfp[0:1000])
-#plt.stem(peaks, np.ones(len(peaks))*2, linefmt='r-', markerfmt='bo', basefmt='r-')
-#plt.savefig("gfp.png")
+#microstates = np.loadtxt("microstates.txt");
+
+plt.clf()
+plt.plot(gfp)
+plt.stem(peaks, np.ones(len(peaks))*2, linefmt='r-', markerfmt='bo', basefmt='r-')
+plt.savefig("gfp.png")
 
 
 chanlocs = loadmat("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/chanlocs.mat", squeeze_me=True)
@@ -54,7 +90,7 @@ rot = np.matrix([[0,-1],[1,0]])
 rot_coords = rot*coords.T
 coords = np.asarray(rot_coords.T) # rotate pi/2 counterclockwise
 
-def topomap(A):
+def topomap(A,name):
     """Plot spatial maps
        Input: A mixing matrix from ICA
        Output: A saved png image of tiled spatial maps"""
@@ -85,7 +121,7 @@ def topomap(A):
         ax.set_aspect('equal', 'datalim')
 
     #plt.title('Microstates?')
-    plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/topomap.png",dpi=600)
+    plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/%s.png" %(name),dpi=600)
 
 def tempomap(intensities, name):
     plt.clf()
@@ -96,34 +132,51 @@ def tempomap(intensities, name):
         ax.plot(range(intensities.shape[1]),intensities[i,:],linewidth=0.2)
         plt.title('IC%d' %(i+1),size=8)
     #plt.title('Temporal components')
-    plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/%s.png" %(name),dpi=600)
+    plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/%s.png" %(name),dpi=1200)
+
+
 
 nICs = 30
-ica = FastICA(n_components=nICs)
+ica = FastICA(n_components=nICs, fun='cube') # 
 S = ica.fit_transform(microstates) # temporal
-A = ica.mixing_ # spatial 
+A = ica.mixing_ # spatial
 print "Done with ICA"
 
-# topomap(A)
+topomap(A, "microstates_cube2")
+# tempomap(A, "temporal")
 
 #The same decomposition matrix was applied to the continuous EEG, which yielded an intensity value for each IC at each time point.
 
-intensities = np.dot(A,eeg.T)
+intensities = np.dot(A,eeg_all.T)
+
+#plt.clf()
+#plt.plot(range(intensities.shape[1]),intensities[13,:],linewidth=0.2)
+#plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/IC14.png",dpi=600)
 
 
 norm_time_course = np.argmax(abs(intensities),0)
-binaryseq = np.zeros(eeg.shape)
+lengths_of_microstates = np.array([ sum( 1 for _ in group ) for key, group in itertools.groupby(norm_time_course) if key ])*2
+
+plt.clf()
+plt.hist(lengths_of_microstates[lengths_of_microstates<200]*2,bins=100)
+plt.savefig("distribution_of_microstate_intervals.png")
+
+binaryseq = np.zeros(eeg_all.shape)
 binaryseq[range(len(norm_time_course)),norm_time_course] = 1
 
 plt.clf()
-for i in range(4):
-    ax = plt.subplot(4,1,i+1)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.plot(range(5000),binaryseq[0:5000,i],linewidth=0.2)
+plt.plot(np.arange(1500),norm_time_course[:1500])
+plt.savefig("binary_microstate_timeseries.png")
+
+#plt.clf()
+#for i in range(4):
+#    ax = plt.subplot(4,1,i+1)
+#    ax.set_xticklabels([])
+#    ax.set_yticklabels([])
+#    ax.plot(range(5000),binaryseq[0:5000,i],linewidth=0.2)
 #    plt.title('IC%d' %(i+1),size=8)
 #plt.title('Temporal components')
-plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/regressors.png",dpi=600)
+#plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/regressors.png",dpi=600)
 
 hrf = loadmat("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/hrf.mat", squeeze_me=True)
 #plt.clf()
@@ -134,14 +187,14 @@ hrf = loadmat("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/hrf.mat",
 regressors = np.apply_along_axis(lambda x: signal.fftconvolve(x,hrf['hrf'],mode='same'), axis=0, arr=binaryseq)
 print "Done with convolution"
 
-plt.clf()
-for i in range(4):
-    ax = plt.subplot(4,1,i+1)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.plot(range(regressors.shape[0]),regressors[:,i],linewidth=0.2)
+#plt.clf()
+#for i in range(4):
+#    ax = plt.subplot(4,1,i+1)
+#    ax.set_xticklabels([])
+#    ax.set_yticklabels([])
+#    ax.plot(range(regressors.shape[0]),regressors[:,i],linewidth=0.2)
 #    plt.title('IC%d' %(i+1),size=8)
 #plt.title('Temporal components')
-plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/regressors.png",dpi=600)
+#plt.savefig("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/regressors.png",dpi=600)
 
 np.savetxt("/home/frans/Dropbox/DTU/AdvancedMachineLearning/Project/regressors.txt", regressors)
